5372fd5353bab6cda8be07c910d76c8697f015f0
Anmol Singh <ASingh2320@outlook.com>
2023-02-11 23:39:09 -0500

    add left scroll

--- src/common_components/Navbar.js
+++ src/common_components/Navbar.js
@@ -7,22 +7,21 @@ import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import SwipeableDrawer from '@mui/material/SwipeableDrawer';
-
+import FormControl from '@mui/material/FormControl';
+import { TextField } from '@mui/material';
 //
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
-import ListItemAvatar from '@mui/material/ListItemAvatar';
-import Avatar from '@mui/material/Avatar';
-import ImageIcon from '@mui/icons-material/Image';
-import WorkIcon from '@mui/icons-material/Work';
-import BeachAccessIcon from '@mui/icons-material/BeachAccess';
+import AddIcon from '@mui/icons-material/Add';
 
 export default function Navbar() {
 
-    const [drawer, toggleDrawer] = useState(true);
+    const [drawer, toggleDrawer] = useState(false);
+    const [leftdrawer, toggleLeft] = useState(false);
 
   return (
+    <>
+    
     <Box position="absolute" sx={{  flexGrow: 1 }}>
       <AppBar sx={{ backgroundColor: "#990000"}}>
         <Toolbar>
@@ -80,8 +79,47 @@ export default function Navbar() {
     </List>
         </SwipeableDrawer>
           
+
+    <SwipeableDrawer
+    anchor ={'left'}
+      open={leftdrawer}
+    PaperProps={{
+        sx: {
+        backgroundColor: "#D9D9D9"
+        }
+    }}
+        >
+    <FormControl>
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" defaultValue="Group Name"/>
+        
+    </FormControl>
+
+    <List sx={{ width: '100%', bgcolor: 'background.paper' , m: 2, backgroundColor:  "#D9D9D9"}}>
+      <ListItem>
+        <Typography sx={{fontSize: 20}}> In Person </Typography>
+      </ListItem>
+      <ListItem>
+      <Typography sx={{fontSize: 20}}> Virtual </Typography>
+      </ListItem>
+      <ListItem>
+      <Typography sx={{fontSize: 20}}> Login </Typography>
+      </ListItem>
+      <ListItem>
+      <Typography sx={{fontSize: 20}}> Register </Typography>
+      </ListItem>
+    </List>
+        </SwipeableDrawer>
+          
         </Toolbar>
+
+        <Button onClick={() => toggleLeft(true)} variant="contained" sx={{position: "fixed", bottom: "20px", left: "10px", fontFamily: '"Helvetica", "Arial", sans-serif',color: "white", background: "#990000", fontWeight: 'bold', marginRight: '10px'}}>
+            <AddIcon/>
+        </Button>
       </AppBar>
+      
     </Box>
+
+
+</>
   )
 }
