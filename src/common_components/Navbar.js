d0dd74a418b0cb34f75632c131c305ddd7332312
Brian Shao <brianshao20@gmail.com>
2023-02-11 22:53:10 -0500

    Login and register modals added. Hamburger fixed

--- src/common_components/Navbar.js
+++ src/common_components/Navbar.js
@@ -3,24 +3,42 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import SwipeableDrawer from '@mui/material/SwipeableDrawer';
-
-//
+import CloseIcon from '@mui/icons-material/Close';
+import Button from '@mui/material/Button';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
-import ListItemAvatar from '@mui/material/ListItemAvatar';
-import Avatar from '@mui/material/Avatar';
-import ImageIcon from '@mui/icons-material/Image';
-import WorkIcon from '@mui/icons-material/Work';
-import BeachAccessIcon from '@mui/icons-material/BeachAccess';
+import LoginModal from './LoginModal';
+import RegisterModal from './RegisterModal';
 
 export default function Navbar() {
 
-    const [drawer, toggleDrawer] = useState(true);
+    const [drawer, toggleDrawer] = useState(false);
+    const [openRegister, setOpenRegister] = useState(false);
+    const [openLogin, setOpenLogin] = useState(false);
+
+    function toggleRegisterModal(){
+      setOpenRegister(true);
+      toggleDrawer(false);
+    }
+
+    function closeHamburger() {
+      toggleDrawer(false);
+    }
+
+    function toggleLoginModal(){
+      setOpenLogin(true);
+      toggleDrawer(false);
+    }
+
+
+    let registerModal = "";
+    if(openRegister) {
+    registerModal = " ";
+    }
+
 
   return (
     <Box position="absolute" sx={{  flexGrow: 1 }}>
@@ -57,31 +75,40 @@ export default function Navbar() {
     <SwipeableDrawer
     anchor ={'right'}
       open={drawer}
-      onBlur={() => toggleDrawer(false)}
+      //onBlur={() => toggleDrawer(false)}
     PaperProps={{
         sx: {
-        backgroundColor: "#D9D9D9"
+        backgroundColor: "#D9D9D9",
+        width:'50vh',
         }
     }}
         >
-    <List sx={{ width: '100%', bgcolor: 'background.paper' , m: 2, backgroundColor:  "#D9D9D9"}}>
+    <List sx={{ width: '60%', bgcolor: 'background.paper' , m: 2, backgroundColor:  "#D9D9D9"}}>
       <ListItem>
-        <Typography sx={{fontSize: 20}}> In Person </Typography>
+      <Button sx={{fontSize: 20, left:'120%'}} onClick={closeHamburger}> 
+      <CloseIcon/> 
+      </Button>
       </ListItem>
       <ListItem>
-      <Typography sx={{fontSize: 20}}> Virtual </Typography>
+      <Button sx={{fontSize: 20}}> In Person </Button>
       </ListItem>
       <ListItem>
-      <Typography sx={{fontSize: 20}}> Login </Typography>
+      <Button sx={{fontSize: 20}} > Virtual </Button>
       </ListItem>
       <ListItem>
-      <Typography sx={{fontSize: 20}}> Register </Typography>
+      <Button sx={{fontSize: 20}} onClick={toggleLoginModal}> Login </Button>
       </ListItem>
+      <ListItem>
+      <Button sx={{fontSize: 20}} onClick={toggleRegisterModal}> Register </Button>
+      </ListItem>
+
     </List>
         </SwipeableDrawer>
           
         </Toolbar>
       </AppBar>
+      {openRegister && <RegisterModal open={openRegister} setOpen={setOpenRegister}/>}
+      {openLogin && <LoginModal open={openLogin} setOpen={setOpenLogin}/>}
     </Box>
   )
 }
